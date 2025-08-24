78892277a75e908928f483b2387731d0ce7306df
gumasaje <geniarti@naver.com>
2025-08-25 03:09:23 +0900

    333

--- src/main/java/com/kimsalee/jimcarry/config/WebConfig.java
+++ src/main/java/com/kimsalee/jimcarry/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://hackathon-7nsg.vercel.app")
+                .allowedOrigins("https://hackathon-7nsg.vercel.app", "http://localhost:3000")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
     }
 }
